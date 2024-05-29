c51faffd4a0c8711018e82800c51e355a4090fd0
elianel <xElianChan@protonmail.com>
2024-05-29 20:03:34 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Disclaimer
+To whom it may concern, ***I do not condone cheating***. This project has ***educational purposes only***.
+
+### External Client Modifications for Unity Games built on [memflow](https://github.com/memflow/memflow)
+
+It's an example speed hack that can work on any ***Unity 2022.3.6f1 IL2CPP*** game. 
+
+As I'm writing this it's still undetected by EasyAntiCheat. 
+
+[Video Showcase on VRChat(Recorded By An Associate (I do not break VRChat's TOS))](https://encfs.just-h.party/view#_WwCmr5pbC=bZyxzDDRHkQSO9UQdUZZU-WfW0m0eKG7qmHvuZ-4826444968663021634)
---
+++ src/main.cpp
@@ -0,0 +1,52 @@
+#include <thread>
+#include <chrono>
+#include <memflow.hpp>
+#include "logger.h"
+#include "memory.h"
+#include "Unity/Unity.h"
+#include "vk_keys.h"
+#include "offsets.h"
+
+Memory *pMemory;
+Unity *pUnity;
+
+int main(int argc, char *argv[])
+{
+        if (argc < 2) {
+            std::cerr << "Invalid Usage! missing argument: <process_name>" << std::endl;
+            return 1; 
+        }
+
+        // log_init(LevelFilter::LevelFilter_Info); //memflow log level
+        Logger::setLogLevel(LogLevel::LOG_DEBUG);
+        Memory mem;
+        mem.open_process(argv[1]);
+        pMemory = &mem;
+
+        Unity unity(mem);
+        if (!unity.init())
+        {
+                Logger::warn("Failed to init unity.");
+                return 1;
+        }
+
+        pUnity = &unity;
+
+        while (true)
+        {
+                std::this_thread::sleep_for(std::chrono::milliseconds(25));
+                if (!pMemory->is_key_pressed(VK_SHIFT))
+                        continue;
+                if (pMemory->is_key_pressed(VK_LEFT))
+                {
+                        Logger::info("Toggled speed hack ON\n");
+                        pUnity->set_time_scale(10.0f);
+                }
+                if (pMemory->is_key_pressed(VK_RIGHT))
+                {
+                        Logger::info("Toggled speed hack OFF\n");
+                        pUnity->set_time_scale(1.0);
+                }
+        }
+        return 0;
+}
